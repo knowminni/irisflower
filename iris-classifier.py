d97ed977638271ae3c9ff10b07ceea2297fd3712
Neha Vishwakarma <envyneha1998.1999@gmail.com>
2021-11-18 23:36:56 +0530

    Update iris-classifier.py

--- iris-classifier.py
+++ iris-classifier.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+from tensorflow import keras
+from keras.models import load_model
 import numpy as np
 from PIL import Image
 
